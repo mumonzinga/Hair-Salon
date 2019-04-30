23c7bbd7d81f1681f920fbcfb06caa8e5b101e8d
Mumo Nzinga <mnzinga888@gmail.com>
2019-04-30 22:05:10 +0300

    add teststylist

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -2,43 +2,45 @@ import org.junit.Rule;
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
-public class ClientTest {
+public class StylistTest {
+
+
     @Rule
     public DatabaseRule database = new DatabaseRule();
 
-    //testing save method
+
+    // testing save method
     @Test
-    public void savingClient_true() {
-        Client client = new Client("Ninja","M","100", 2);
-        client.save();
-        assertEquals("Ninja", client.getName());
+    public void savingStylist_true() {
+        Stylist stylist = new Stylist("Alpha","M","500");
+        stylist.save();
+        assertEquals("Alpha", stylist.getName());
     }
-    // find client test
+    // testing for update method
     @Test
-    public void findCLient_true() {
-        Client client = new Client("Ninja","M","100", 2);
-        client.save();
-        assertEquals("Ninja", Client.find(client.getId()).getName());
+    public void updatingStylist_true() {
+        Stylist stylist = new Stylist("Ninja","M","500");
+        stylist.save();
+        stylist.update("Brandon", "M", "100");
+        assertEquals("Brandon", Stylist.find(stylist.getId()).getName());
+        assertEquals("M", Stylist.find(stylist.getId()).getGender());
+        assertEquals("100", Stylist.find(stylist.getId()).getContact());
     }
-    // updating client test
+    //find stlist test
     @Test
-    public void updatingClient_true() {
-        Client client = new Client("Ninja","M","100", 2);
-        client.save();
-        client.update("Brandon", "M", "100", 2);
-        assertEquals("Brandon", Client.find(client.getId()).getName());
-        assertEquals("M", Client.find(client.getId()).getGender());
-        assertEquals("100", Client.find(client.getId()).getContact());
-        assertEquals(2, Client.find(client.getId()).getStylist());
+    public void findStylist_true() {
+        Stylist stylist = new Stylist("Alpha","M","999");
+        stylist.save();
+        assertEquals("Alpha", Stylist.find(stylist.getId()).getName());
     }
-    //delete client test
+    //delete stylist test
     @Test
-    public void delete_deletesClient_true() {
-        Client client = new Client("Ninja","M","100", 2);
-        client.save();
-        int clientId = client.getId();
-        client.delete();
-        assertEquals(null, Client.find(clientId));
+    public void delete_deletesStylist_true() {
+        Stylist stylist = new Stylist("Alpha", "M", "999");
+        stylist.save();
+        int stylistId = stylist.getId();
+        stylist.delete();
+        assertEquals(null, Stylist.find(stylistId));
     }
 
 }
